c711eedea15c74c44f0933c12af453e3157ecf35
Geralt-Of-Rivia-Witcher <singhsiddhantkumar@gmail.com>
2023-04-14 23:13:20 +0530

    refactor: :recycle: Updated Projects section

--- src/const.js
+++ src/const.js
@@ -1,52 +1,50 @@
 export const project_list = [
-    {
-        project_name: "HOLOCRON",
-        project_description:
-            "A safe and secure password manager with AES-256-CBC encryption, password salting and hashing and token-based authentication.",
-        project_link_1: "https://github.com/Geralt-Of-Rivia-Witcher/Holocron/",
-        project_link_2: "https://holocron.siddhantkumarsingh.me/",
-        class: "card first",
-    },
-    {
-        project_name: "GAMOPEDIA",
-        project_description:
-            "An encyclopedia of Video Games. A web app providing comprehensive video game information.",
-        project_link_1:
-            "https://github.com/Geralt-Of-Rivia-Witcher/Gamopedia_Frontend/",
-        project_link_2: "https://gamopedia.siddhantkumarsingh.me/",
-        class: "card second",
-    },
-    {
-        project_name: "BANK LANDING PAGE",
-        project_description:
-            "A Bank Landing Page integrated with currency converter API.",
-        project_link_1:
-            "https://github.com/Geralt-Of-Rivia-Witcher/Bank_Landing_Page/",
-        project_link_2: "https://my-bank-frontend.vercel.app/",
-        class: "card third",
-    },
-    {
-        project_name: "CANVAS",
-        project_description:
-            "This website, which was inspired by draw.io, allows us to quickly and easily alter a number of text fields and buttons by dragging and resizing them.",
-        project_link_1: "https://github.com/Geralt-Of-Rivia-Witcher/Canvas/",
-        project_link_2: "https://canvas4all.netlify.app/",
-        class: "card fourth",
-    },
-    {
-        project_name: "VIGENERE CIPHER",
-        project_description:
-            "It can encrypt text with keys based on user's choice. It can also decrypt encrypted text with support for 8 languages.",
-        project_link_1:
-            "https://github.com/Geralt-Of-Rivia-Witcher/VigenereCipher",
-        class: "card fifth",
-    },
-    {
-        project_name: "THIS PORTFOLIO WEBSITE",
-        project_description:
-            "This website to tell a little bit about who I am.",
-        project_link_1:
-            "https://github.com/Geralt-Of-Rivia-Witcher/portfolio_2.0/",
-        class: "card sixth",
-    },
+  {
+    project_name: "DARKHOLD",
+    project_description:
+      "Unbreakable cloud storage with unique Hybrid Cryptography. Protect your files with top-notch encryption for unparalleled security.",
+    project_link_1: "https://github.com/Geralt-Of-Rivia-Witcher/Darkhold/",
+    project_link_2: "https://darkhold.siddhantkumarsingh.me/",
+    class: "card first",
+  },
+  {
+    project_name: "HOLOCRON",
+    project_description:
+      "Your reliable password manager. Keep your passwords and other sensitive information safe with advanced encryption and a user-friendly interface.",
+    project_link_1: "https://github.com/Geralt-Of-Rivia-Witcher/Holocron/",
+    project_link_2: "https://holocron.siddhantkumarsingh.me/",
+    class: "card second",
+  },
+  {
+    project_name: "GAMOPEDIA",
+    project_description:
+      "Your ultimate source for comprehensive video game information. From classic titles to the latest releases, it provides detailed video-game information.",
+    project_link_1:
+      "https://github.com/Geralt-Of-Rivia-Witcher/Gamopedia_Frontend/",
+    project_link_2: "https://gamopedia.siddhantkumarsingh.me/",
+    class: "card third",
+  },
+  {
+    project_name: "BANK LANDING PAGE",
+    project_description:
+      "A Bank Landing Page integrated with currency converter API.",
+    project_link_1:
+      "https://github.com/Geralt-Of-Rivia-Witcher/Bank_Landing_Page/",
+    project_link_2: "https://my-bank-frontend.vercel.app/",
+    class: "card fourth",
+  },
+  {
+    project_name: "CANVAS",
+    project_description:
+      "This website, which was inspired by draw.io, allows us to quickly and easily alter a number of text fields and buttons by dragging and resizing them.",
+    project_link_1: "https://github.com/Geralt-Of-Rivia-Witcher/Canvas/",
+    project_link_2: "https://canvas4all.netlify.app/",
+    class: "card fifth",
+  },
+  {
+    project_name: "THIS PORTFOLIO WEBSITE",
+    project_description: "This website to tell a little bit about who I am.",
+    project_link_1: "https://github.com/Geralt-Of-Rivia-Witcher/portfolio_2.0/",
+    class: "card sixth",
+  },
 ];
